d0158b46fff72a8fe62e01518357069a2981e608
Mateus Arruda <mateusarruda@alu.ufc.br>
2018-07-07 18:25:19 -0300

    Adicionado Contato com FormsPree

--- js/functions.js
+++ js/functions.js
@@ -211,6 +211,7 @@ particlesJS.load('particles-js4','../js/particles.json',function(){
 });
 console.log($('#formNome')[0].value);
 document.getElementById("btnEnvia").addEventListener("click", function(event){
+
 if (($('#formNome')[0].value=="") || ($('#formEmail')[0].value=="") || ($('#formMensagem')[0].value=="")) {
   event.preventDefault();
   M.toast({html: 'Um dos campos não foi preenchido!'});
